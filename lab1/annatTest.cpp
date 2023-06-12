816b0e069127d5639bbd1e151ec352979e7477b8
emillantz <emil.lantz@gmail.com>
2023-06-12 12:27:32 +0200

    fixed 'annatTest.cpp', added Makefile to avoid very long gcc commands every compile and added md-file to answer lab question

--- lab1/annatTest.cpp
+++ lab1/annatTest.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-
+int main()
+{
   int a;
   int b = 12;
-  a = 0
+  a = 0; // Missing semicolon on ln8 => error: expected ';' after expression
 
   cout << "a = " << a << endl;
-  cout << a+b << '\n' << "a-b = " << a-b << endl;
+  cout << a + b << '\n'
+       << "a-b = " << a - b << endl;
 
   return 0;
 }
---
+++ lab1/rapport.md
@@ -0,0 +1,3 @@
+# Inlämningsuppgift 1
+
+Anledningen till att programmet inte kunde kompilera var att det saknades ett semikolon efter "a = 0" (på rad 8).
